e9b75ced80dfcdefe2686f5f88a0a924da28a91c
aravindsabu <aravindsabu2001@gmail.com>
2022-10-19 08:52:17 +0530

    ok

---
+++ src/main/java/com/nestdigital/xyzbaclend/app/XyzBaclendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzbaclend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBaclendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzBaclendAppApplication.class, args);
+	}
+
+}
